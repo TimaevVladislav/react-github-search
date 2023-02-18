a22b5cf6e2cee448d4053f6b79efb0de6a9be9f6
Vladislav <timaevvladislav@gmail.com>
2023-02-18 15:58:33 +0600

    init

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -21,7 +21,6 @@ export const RepoCard = ({ repo }: { repo: IRepo }) => {
         removeFavorite(repo.html_url)
     }
 
-
     return (
         <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
             <a href={repo.html_url} target="_blank">
--- src/hooks/debounce.ts
+++ src/hooks/debounce.ts
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react"
 
+// Задержка отправки запросов к серверу
 export function useDebounce(value: string, delay: number = 300) {
     const [debounced, setDebounced] = useState(value)
 
